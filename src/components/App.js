01927468241ee96dae2c703fa54195fadfa1d82a
Tushar kumar <narutoboi41@gmail.com>
2026-04-19 20:00:07 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -26,13 +26,13 @@ const App = () => {
       <div className="Calculator">
         <input value={val} className="cal box-1" type="text" />
         <button onClick={handleValue} className="cal box-2" id="btn-9">C</button>
-        <button onClick={handleValue} className="cal box-3" id="btn-9">/</button>
+        <button onClick={handleValue} className="cal box-3" id="btn-9 btn-div">/</button>
         <button onClick={handleValue} className="cal box-4">*</button>
-        <button onClick={handleValue} className="cal box-5" id="btn-9">-</button>
+        <button onClick={handleValue} className="cal box-5" id="btn-9 btn--">-</button>
         <button onClick={handleValue} className="cal box-5">7</button>
         <button onClick={handleValue} className="cal box-7">8</button>
         <button onClick={handleValue} className="cal box-8">9</button>
-        <button onClick={handleValue} className="cal box-9" id='btn-5'>+</button>
+        <button onClick={handleValue} className="cal box-9" id='btn-5 plus'>+</button>
         <button onClick={handleValue} className="cal box-10">4</button>
         <button onClick={handleValue} className="cal box-11">5</button>
         <button onClick={handleValue} className="cal box-12">6</button>
