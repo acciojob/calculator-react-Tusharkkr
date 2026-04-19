80e033b4952bd695a4c530c5033e5b26e2053c83
Tushar kumar <narutoboi41@gmail.com>
2026-04-19 19:50:29 +0530

    chk

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,51 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+{/* <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p> */ }
 
+import React, { useState } from "react";
+import './../styles/App.css';
+
+
+
+const App = () => {
+
+  let [val, setVal] = useState('')
+
+  function handleValue(e) {
+    let value = e.target.innerText
+    // console.log(value)
+    if(value == 'C'){
+      setVal('')
+    }else if(value == '='){
+      setVal(eval(val))
+    }else{
+      setVal(val + value)
+    } 
+  }
+
+  return (
+    <div className="main-container">
+      <div className="Calculator">
+        <input value={val} className="cal box-1" type="text" />
+        <button onClick={handleValue} className="cal box-2">C</button>
+        <button onClick={handleValue} className="cal box-3">/</button>
+        <button onClick={handleValue} className="cal box-4">*</button>
+        <button onClick={handleValue} className="cal box-5">-</button>
+        <button onClick={handleValue} className="cal box-5">7</button>
+        <button onClick={handleValue} className="cal box-7">8</button>
+        <button onClick={handleValue} className="cal box-8">9</button>
+        <button onClick={handleValue} className="cal box-9">+</button>
+        <button onClick={handleValue} className="cal box-10">4</button>
+        <button onClick={handleValue} className="cal box-11">5</button>
+        <button onClick={handleValue} className="cal box-12">6</button>
+        <button onClick={handleValue} className="cal box-13">1</button>
+        <button onClick={handleValue} className="cal box-14">2</button>
+        <button onClick={handleValue} className="cal box-15">3</button>
+        <button onClick={handleValue} className="cal box-16">=</button>
+        <button onClick={handleValue} className="cal box-17">0</button>
+        <button onClick={handleValue} className="cal box-18">.</button>
+        <button onClick={handleValue} className="cal box-19"></button>
+      </div>
+    </div>
+  )
+}
+
+export default App
